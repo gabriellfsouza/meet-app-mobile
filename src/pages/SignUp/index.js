4eab7b253a8dec17683ffd1b5e4588a6403f3739
Gabriel Lima <gabriellfsouza@gmail.com>
2019-08-20 05:21:24 -0300

    chore: fisrt components (button, textinput, routing components, background)

---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {} from 'react-native';
+
+import Background from '~/components/Background';
+
+// import { Container } from './styles';
+
+export default function SignUp() {
+  return <Background />;
+}
