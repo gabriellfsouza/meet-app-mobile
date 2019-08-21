e5128fb368d96187377b5859f219bbf6ecf12e5d
Gabriel Lima <gabriellfsouza@gmail.com>
2019-08-21 06:24:03 -0300

    conf: configuration file and redux/redux-saga features included

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
 import { StatusBar } from 'react-native';
 
-// import './config/ReactotronConfig';
+import './config/ReactotronConfig';
 
+import { store, persistor } from './store';
 import Routes from './routes';
 
-const App = () => {
+const Index = () => {
   return (
-    <>
-      <StatusBar barStyle="light-content" backgroundColor="#22202d" />
-      <Routes />
-    </>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <StatusBar barStyle="light-content" backgroundColor="#22202d" />
+        <Routes />
+      </PersistGate>
+    </Provider>
   );
 };
 
-export default App;
+export default Index;
